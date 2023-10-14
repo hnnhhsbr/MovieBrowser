4af359233e6ef5b999dbb7289a9b6d452d8e62e8
Hannah Ahmad Sabri <hannahasabri@gmail.com>
2023-10-14 12:32:13 +0800

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Hannah Ahmad Sabri on 14/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
